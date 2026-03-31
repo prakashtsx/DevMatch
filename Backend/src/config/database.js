278fbd0d677e0a794faf50d371d520d6897f1d84
Prakash <urprakashgupta@gmail.com>
2026-03-31 15:29:36 +0530

    feat: update mongodb connection string

--- Backend/src/config/database.js
+++ Backend/src/config/database.js
@@ -1,9 +1,8 @@
 import mongoose from "mongoose";
-import { DB_NAME } from "../constants.js";
 
 const connectDB = async () => {
     try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}`);
         console.log(`MongoDB connected: ${connectionInstance.connection.host}`);
     } catch (error) {
         console.error("Database connection error:", error);
