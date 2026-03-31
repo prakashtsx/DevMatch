61e6ee574e053186fd0285c95e26ab51eb5159b5
Prakash <urprakashgupta@gmail.com>
2026-03-31 16:15:05 +0530

    feat: integrate Tailwind CSS into the project and update index.html title

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,9 +2,7 @@ import "./App.css";
 
 function App() {
   return (
-    <>
-      <h1>Hello world</h1>
-    </>
+    <h1 class="text-3xl font-bold underline text-red-500">Hello world!</h1>
   );
 }
 
